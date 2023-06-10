7d9d38529cca87b402c44ea0d833ce97f034ca6b
Andrew Eubanks <aeubanksguitar@gmail.com>
2023-06-10 09:50:19 -0400

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro totam adipisci sint ab nemo aperiam quis labore, beatae ex consectetur minus ducimus qui dolore asperiores fugiat repudiandae. Unde, tenetur accusamus',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro totam adipisci sint ab nemo aperiam quis labore, beatae ex consectetur minus ducimus qui dolore asperiores fugiat repudiandae. Unde, tenetur accusamus',
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro totam adipisci sint ab nemo aperiam quis labore, beatae ex consectetur minus ducimus qui dolore asperiores fugiat repudiandae. Unde, tenetur accusamus',
+  },
+];
+
+export default FeedbackData;
